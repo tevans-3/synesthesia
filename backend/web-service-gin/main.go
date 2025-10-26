250ea4c450753b88d3406550dbdf60be6005dc2c
Thomas Evans <tevans3@ualberta.ca>
2025-10-25 18:50:04 -0600

    ..

--- backend/web-service-gin/main.go
+++ backend/web-service-gin/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
-
+	"github.com/gin-contrib/cors"
+	"fmt"
 )
 
 type hexCode struct { 
 	HEX string `json:"hex"` 
+	USERID string `json:"userId"`
 }
 
 var hexCodes []hexCode
@@ -22,13 +23,28 @@ func postHexCode(c *gin.Context) {
  
 	hexCodes = append(hexCodes, newHex)
 
-	c.IndentedJSON(http.StatusCreated, newHex) 
+	c.IndentedJSON(http.StatusCreated, newHex)
+ 
+	fmt.Print(hexCodes)
+}
 
+func getHexCodes(c *gin.Context){
+	c.IndentedJSON(http.StatusOK, hexCodes)
 }
 
 func main() {
-	router := gin.Default()
+	router := gin.Default() 
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:64381"}, 
+		AllowMethods: []string{"POST", "GET"}, 
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		ExposeHeaders: []string{"*"},
+	}))
+//	router.Use(cors.Default())
+  router.GET("/getHexCodes", getHexCodes)
   router.POST("/postHexCode", postHexCode)
 
-	router.Run("localhost:64381")
+	router.Run("localhost:8080")
+
+	
 }
