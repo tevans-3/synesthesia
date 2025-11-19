c8a99f7a35d553ed2654a4d6620b53e750681fce
Thomas Evans <tevans3@ualberta.ca>
2025-11-18 23:47:46 -0700

    more tone.js

--- app/src/layout/MainLayout.jsx
+++ app/src/layout/MainLayout.jsx
@@ -41,6 +41,8 @@ export default function MainLayout() {
     const [saturationWidth, setSaturationWidth] = useState(null); 
     const [saturationHeight, setSaturationHeight] = useState(null);
 
+    const [status, setStatus] = useState(null); 
+
     const N = 24; //number of equal divisions in the 24 TET scale
     const A = 440; //frequency of reference note A 
     var notes = []; 
@@ -55,13 +57,12 @@ export default function MainLayout() {
       var notesInChord = []; 
       hexCode = hexCode.replace('#', '');
       var num = parseInt(hexCode, 16);
-      console.log(num);  
+       
       var i = 0; 
       while (num) {
-        console.log(num);
+        
         if (num & 1) {
           i ++;
-          console.log(notes[i]);
           notesInChord.push(notes[i]); 
         }
         num = num >> 1; 
@@ -70,14 +71,14 @@ export default function MainLayout() {
     } 
 
     function GenerateAudio(hexCode){
-      console.log(hexCode);
       generate24TetScale();
-      
+      Tone.start(); 
+      console.log("happening");
       var chord = getChord(hexCode); 
-      console.log(chord);
-      const synth = new Tone.PolySynth(Tone.Synth).toDestination(); 
-      const now = Tone.now(); 
-      synth.triggerRelease(chord, now); 
+      const synth = new Tone.PolySynth(Tone.Synth, {oscillator:{type:"sine"}}).toDestination(); 
+      const now = Tone.now();
+      
+      synth.triggerAttackRelease(chord, 1); 
       
     } 
 
@@ -100,23 +101,49 @@ export default function MainLayout() {
       console.error('Error: ', error);
     }
    }
-    function handleChange(newColor) {
+    async function handleChange(newColor) {
+      await Tone.start(); 
+      setColor(newColor);
        
-      setColor(newColor); 
       setHsva({ h:newColor.h, s:newColor.s, v:newColor.v, a: hsva.a });
-      console.log(newColor);
+      setHex(hsvaToHex(hsva)); 
+     
+      GenerateAudio(hex); 
       
-    };
+      
+    }
+    const debounce = (callback, arg, delay) => {
+      let timer; 
+      return function() {
+        clearTimeout(timer); 
+        timer = setTimeout(() => {
+          callback(arg); 
+        }, delay)
+      }
+    }
+    const debouncedHandleChange = debounce(handleChange, hex, 5000); 
+  
+      /*setTimeout(() => {
+      GenerateAudio(hsvaToHex(hsva));}, 50);
+    };*/
 
-    useEffect(() => 
+    async function handleClick() { 
+      await Tone.start(); 
+    }
+    
+
+    /*useEffect(() => 
     {
       const hexVal = hsvaToHex(hsva); 
+      
       setHex(hexVal); 
+       
+      
       //PostHexCode(JSON.stringify({hexVal}));
-      setTimeout(() => {
-        GenerateAudio(hexVal)
-      }, 2500); 
-    }, [hsva]); 
+      
+      GenerateAudio(hexVal)
+      
+    }, [hsva]); */
 
 
     return (
@@ -128,7 +155,7 @@ export default function MainLayout() {
                      <Typography variant={large ? "b1" : medium ? "b1" : small ? "b2" : "b2"} 
                       color={'#000'} 
                       sx={{paddingLeft:"15px", marginLeft:"auto", whiteSpace:"nowrap"}}>
-                        Make music with colors. 
+                      
                      </Typography>
                     <List sx={{ paddingLeft: '5.8em'} }>
                         <ListItem button component={Link} to="https://github.com/tevans-3/synesthesia">
